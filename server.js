70c91fbd8cc4345588d36b9f431e938cebfaebcb
meera-ramesh19 <meeraramesh19@outlook.com>
2022-07-21 13:39:03 -0400

    removed node modules

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = require('./app');
 require('dotenv').config();
+const cors = require('cors');
 
 const  PORT  = process.env.PORT || 8080;
 app.listen(PORT, () => {
