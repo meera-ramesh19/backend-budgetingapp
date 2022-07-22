9701d93c470be53f992fdcd3e4acda6a709b78b2
meera-ramesh19 <meeraramesh19@outlook.com>
2022-07-22 12:38:23 -0400

    updated modelinfo

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,47 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const transactions = require('../models/transactions');
+const transactionArray = require('../models/transactions');
 
-// router.use('/:id', (req, res, next) => {
-//   if (!transactions[req.params.id]) {
-//     res.status(404).send('Not Found');
-//   } else {
-//     next();
-//   }
-// });
+router.use('/:id', (req, res, next) => {
+  if (!transactionArray[req.params.id]) {
+    res.status(404).send('Not Found');
+  } else {
+    next();
+  }
+});
 
 // Index
 router.get('/', (req, res) => {
   console.log('hello');
-  res.json(transactions);
+  res.json(transactionArray);
 });
 
 // Show
 router.get('/:id', (req, res) => {
-  console.log(req.params, transactions);
-  res.json(transactions[req.params.id]);
+  console.log(req.params, transactionArray);
+  res.json(transactionArray[req.params.id]);
 });
 
 // Create
 router.post('/', (req, res) => {
-  transactions.push(req.body);
-  res.json(transactions[transactions.length - 1]);
+  transactionArray.push(req.body);
+  res.json(transactionArray[transactionArray.length - 1]);
 });
 
 // UPDATE
 router.put('/:id', (req, res) => {
   const { id } = req.params;
   const newData = req.body;
-  transactions[id] = newData;
-  res.send(transactions[id]);
+  transactionArray[id] = newData;
+  res.send(transactionArray[id]);
 });
 
 //DELETE
 router.delete('/:id', (req, res) => {
-
   const { id } = req.params;
-  transactions.splice(id, 1);
-  res.send(transactions);
+  transactionArray.splice(id, 1);
+  res.send(transactionArray);
 });
-
-module.exports = router;
