11998ba316f830134baa30dd03ebcbb2b44eacc8
meera-ramesh19 <meeraramesh19@outlook.com>
2022-08-01 12:14:56 -0400

    commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+
+
 const express = require('express');
 const app = require('./app');
 require('dotenv').config();
 const cors = require('cors');
 
-const  PORT  = process.env.PORT || PORT;
+const PORT = process.env.PORT || PORT;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
