1f8c06e14b2558e165ece93546bc2a67be696eb3
meera-ramesh19 <meeraramesh19@outlook.com>
2022-08-09 18:28:39 -0400

    works with database

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
+const path = require('path');
 const express = require('express');
-const app = require('./app');
-require('dotenv').config();
-const cors = require('cors');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
 
-const  PORT  = process.env.PORT || PORT;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+connectDB();
+
+const app = express();
+const cors = require('cors');
+// cors all request should go through this so that browser should
+//trusts the requests
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/transactions', require('./routes/transactionRoutes'));
+// app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+//     )
+//   );
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'));
+// }
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
