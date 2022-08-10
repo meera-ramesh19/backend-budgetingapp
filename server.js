082849f4e77f62ad189f2120aba33640d64592dc
meera-ramesh19 <meeraramesh19@outlook.com>
2022-08-10 00:19:44 -0400

    deleted extra port

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-
 // const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -34,7 +32,6 @@ app.use('/api/transactions', require('./routes/transactionRoutes'));
 // }
 
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || PORT;
 app.listen(PORT, () => {
